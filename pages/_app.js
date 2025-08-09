9bc6449e0b008c1ba7ec4101c7a9df40797f6c4a
Moldovan Csaba <moldovancsaba@gmail.com>
2025-08-09 03:56:33 +0200

    feat: add bottom info bar with SEYU logo and text

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,19 @@
 import '../styles/globals.css';
+import Image from 'next/image';
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <div className="info-bar">
+        <Image
+          src="https://i.ibb.co/nsmDf93m/seyu-logo.png"
+          alt="SEYU Logo"
+          width={48}
+          height={48}
+        />
+        <span className="info-text">SEYU SELFIES</span>
+      </div>
+    </>
+  );
 }
