6c91dc94b8dd207d899a8dcdbfc977ef7fc7a780
Moldovan Csaba <moldovancsaba@gmail.com>
2025-08-09 19:57:21 +0200

    fix: use plain <img> in info bar to avoid build-time element error

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,15 @@
 import '../styles/globals.css';
-import Image from 'next/image';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
       <div className="info-bar">
-        <Image
+        <img
           src="https://i.ibb.co/nsmDf93m/seyu-logo.png"
           alt="SEYU Logo"
-          width={48}
-          height={48}
+          width="48"
+          height="48"
         />
         <span className="info-text">SEYU SELFIES</span>
       </div>
